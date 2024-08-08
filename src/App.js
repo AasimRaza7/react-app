78c2f983fde700351a65bdb07b97f49c346f942f
Aasim Raza <81436069+AasimRaza7@users.noreply.github.com>
2024-08-08 16:59:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
 
-          Welcome to DevOps Session
+          Welcome to DemoApp
       </header>
     </div>
   );
