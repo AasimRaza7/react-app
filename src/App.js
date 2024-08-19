04f09f5f7891beff4f17d9334cde0b4b07f863e8
aasim <aasim.raza@nanosofttek.com>
2024-08-19 13:58:17 +0500

    new change in comment

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 
-          {/* Welcome to DemoApp */}
+          {/* Welcome to Demo App */}
       </header>
     </div>
   );
